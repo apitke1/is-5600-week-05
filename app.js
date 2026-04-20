4d52b34508cb68b099d885b06e51035b990b5958
apitke1 <atharva.pitke@slu.edu>
2026-04-20 03:23:33 +0000

    Lab 5

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
 
-
 // Set the port
 const port = process.env.PORT || 3000
 // Boot the app
@@ -14,11 +13,19 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 app.use(middleware.cors)
 app.get('/', api.handleRoot)
+
+// Product routes
 app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+
+// Order routes
+app.get('/orders', api.listOrders)
+app.post('/orders', api.createOrder)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-
